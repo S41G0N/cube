96ae0e56176f4c7b7e1b01f5e7b6b0a5e2e69a34
Michal Vu <sa1g0n@tuta.io>
2024-05-06 20:02:16 +0200

    Rewriting code + Readme Update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Spinning Cube
-exploring matrices with a spinning cube in C, inspired by a YouTube video made by Servet Gulnaroglu.
+# Spinning Cube
+
+exploring matrices with a spinning cube in C, inspired by a YouTube video made by Servet Gulnaroglu, from which I forked the project. This project was designed for myself as a lesson to understand the implementation of rotation matrices.
+
+## Original Source
+
+[key]: https://www.youtube.com/watch?v=p09i_hoFdd0
--- cube.c
+++ cube.c
@@ -3,76 +3,72 @@
 #include <unistd.h>
 #include <math.h>
 
-float A, B, C;
-float cubeWidth = 20;
-int width = 160, height = 44;
-float zBuffer[160 * 44];
-char buffer[160 * 44];
-int backgroundASCIICode = ' ';
-float incrementSpeed = 0.6;
-float x,y,z;
-int distanceFromCam = 100;
-float ooz;
-int xp, yp;
-float K1 = 40;
-int idx;
+#define WIDTH 160
+#define HEIGHT 44
+#define CUBE_WIDTH 20
+#define INCREMENT_SPEED 0.6
+#define DISTANCE_FROM_CAM 100
+#define K1 40.0
 
-float calculateX(int i, int j, int k){
-    return j*sin(A)*sin(B)*cos(C) - k*cos(A)*sin(B)*cos(C)
-        + j*cos(A)*sin(C) + k*sin(A)*sin(C)+ i*cos(B)*cos(C);
+float A = 0, B = 0, C = 0; // Rotation angles about the axes
+float zBuffer[WIDTH * HEIGHT];
+char buffer[WIDTH * HEIGHT];
+
+float calculateX(int i, int j, int k) {
+    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C)
+        + j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
-float calculateY(int i, int j, int k){
-    return j*cos(A)*cos(C) + k*sin(A)*cos(C) - j*sin(A)*sin(B)*sin(C)
-        + k*cos(A)*sin(B)*sin(C) - i*cos(B)*sin(C);
+float calculateY(int i, int j, int k) {
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) - j * sin(A) * sin(B) * sin(C)
+        + k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
 }
 
-float calculateZ(int i, int j, int k){
-    return k*cos(A)*cos(B) - j*sin(A)*cos(B) + i*sin(B);
+float calculateZ(int i, int j, int k) {
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
-void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch){
-   x = calculateX(cubeX, cubeY, cubeZ);
-   y = calculateY(cubeX, cubeY, cubeZ);
-   z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
-   ooz = 1/z;
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+    float x = calculateX(cubeX, cubeY, cubeZ);
+    float y = calculateY(cubeX, cubeY, cubeZ);
+    float z = calculateZ(cubeX, cubeY, cubeZ) + DISTANCE_FROM_CAM;
+    float ooz = 1 / z;
 
-   xp = (int)(width/2 - 2 * cubeWidth + K1 * ooz * x * 2);
-   yp = (int)(height/2 + K1 * ooz * y);
+    int xp = (int)(WIDTH / 2 - 2 * CUBE_WIDTH + K1 * ooz * x * 2);
+    int yp = (int)(HEIGHT / 2 + K1 * ooz * y);
 
-   idx = xp + yp * width;
-
-   if (idx >= 0 && idx < width * height){
-    if (ooz > zBuffer[idx]){
-        zBuffer[idx] = ooz;
-        buffer[idx] = ch;
-    }     
-   }
+    int idx = xp + yp * WIDTH;
+    if (idx >= 0 && idx < WIDTH * HEIGHT) {
+        if (ooz > zBuffer[idx]) {
+            zBuffer[idx] = ooz;
+            buffer[idx] = ch;
+        }
+    }
 }
 
-int main(){
-    printf("\x12[2J]");
-    while(1){
-       memset(buffer, backgroundASCIICode, width * height);
-       memset(zBuffer, 0, width * height * 4);
-       for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed){
-           for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed){
-               calculateForSurface(cubeX, cubeY, -cubeWidth, '.');
-               calculateForSurface(cubeWidth, cubeY, cubeX, '$');
-               calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
-               calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
-               calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
-               calculateForSurface(cubeX, cubeWidth, cubeY, '+');
-
-           }
-       }
-       printf("\x1b[H]");
-       for (int k = 0; k < width * height; k++){
-           putchar(k % width ? buffer[k] : 10);
-       }
-       A += 0.005;
-       B += 0.005;
-       usleep(1000);
+int main() {
+    printf("\x1b[2J"); // Clear the screen
+    while (1) {
+        memset(buffer, ' ', WIDTH * HEIGHT);
+        memset(zBuffer, 0, sizeof(zBuffer));
+        for (float cubeX = -CUBE_WIDTH; cubeX < CUBE_WIDTH; cubeX += INCREMENT_SPEED) {
+            for (float cubeY = -CUBE_WIDTH; cubeY < CUBE_WIDTH; cubeY += INCREMENT_SPEED) {
+                calculateForSurface(cubeX, cubeY, -CUBE_WIDTH, '.');
+                calculateForSurface(CUBE_WIDTH, cubeY, cubeX, '$');
+                calculateForSurface(-CUBE_WIDTH, cubeY, -cubeX, '~');
+                calculateForSurface(-cubeX, cubeY, CUBE_WIDTH, '#');
+                calculateForSurface(cubeX, -CUBE_WIDTH, -cubeY, ';');
+                calculateForSurface(cubeX, CUBE_WIDTH, cubeY, '+');
+            }
+        }
+        printf("\x1b[H"); // Move cursor to home position
+        for (int k = 0; k < WIDTH * HEIGHT; k++) {
+            putchar(k % WIDTH ? buffer[k] : '\n');
+        }
+        A += 0.005;
+        B += 0.005;
+        usleep(1000);
     }
     return 0;
 }
+
